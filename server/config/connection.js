cd2cb0574a3dcabcc857dea120f4a54ae21f2ba7
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-07-31 18:43:35 -0500

    heroku fixes?

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
+const password = process.env.MONGODB_PASSWORD;
+
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks",
+  process.env.MONGODB_URI ||
+    `mongodb+srv://TinyVail:${password}@cluster0.nf8bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
